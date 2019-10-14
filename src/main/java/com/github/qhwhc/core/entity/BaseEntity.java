783cc1cfeaec1cbf89d6b36e220275330f70bdec
cyb <304136830@qq.com>
2019-10-14 10:35:52 +0800

    mybatis-plus修正

--- src/main/java/com/github/qhwhc/core/entity/BaseEntity.java
+++ src/main/java/com/github/qhwhc/core/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package com.github.qhwhc.core.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -20,6 +21,7 @@ import java.io.Serializable;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
+@ApiModel
 public class BaseEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 
